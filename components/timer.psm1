2591d3bbc5a30640931d39a5fa897e7d04a8035a
Paul Sammut <paulesammut@gmail.com>
2019-02-12 13:51:26 +1100

    timer in ms and s

--- components/timer.psm1
+++ components/timer.psm1
@@ -18,8 +18,16 @@ function PowerPromptTimer {
 
 function ShowTimer {
     $previousCommand = Get-History -Count 1
+    $previousCommandDurationText = ""
     if ("$($previousCommand.Id)" -ne "$savedCommandId") {
         $previousCommandDuration = [int]$previousCommand.Duration.TotalMilliseconds
+        if ($previousCommandDuration -ge 1000) {
+            $previousCommandDuration = [math]::round([double]$previousCommand.Duration.TotalSeconds,2)
+            $previousCommandDurationText = "$previousCommandDuration s"
+        }
+        else {
+            $previousCommandDurationText = "$previousCommandDuration ms"
+        }
         $script:savedCommandId = $previousCommand.Id
     }
 
@@ -27,8 +35,8 @@ function ShowTimer {
         Write-Host
         # Write-Host -NoNewLine "    " -foregroundColor "Yellow"
         Write-Host -NoNewline ("{0:HH}:{0:mm}:{0:ss} " -f (Get-Date)) -foregroundColor "DarkGray"
-        if ($previousCommandDuration) {
-            Write-Host -NoNewLine "($previousCommandDuration ms)"
+        if ($previousCommandDurationText) {
+            Write-Host -NoNewLine "($previousCommandDurationText)"
         }
         Write-Host
     }
